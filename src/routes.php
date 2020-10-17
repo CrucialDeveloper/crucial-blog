a6dfb7ed76bb4e7c097385c41c2ac19b73fc52e3
johnlowery <sbcman74@gmail.com>
2020-10-17 16:09:44 -0500

    added web middleware

--- src/routes.php
+++ src/routes.php
@@ -6,6 +6,6 @@ use CrucialDeveloper\CrucialBlog\PostController;
 
 
 Route::prefix(null !== config('crucial-blog.route_path') ? config('crucial-blog.route_path') : 'blog')->group(function () {
-    Route::get('/', [PostController::class, 'index']);
-    Route::get('{slug}', [PostController::class, 'show']);
+    Route::get('/', [PostController::class, 'index'])->middleware('web');
+    Route::get('{slug}', [PostController::class, 'show'])->middleware('web');
 });
